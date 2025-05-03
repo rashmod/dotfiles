aa8eae1e344d1c3b5eef9468493abd4ef2ef7f00
Bhushan <alhatbhushan@gmail.com>
2025-05-03 13:32:03 +0530

    style inactive window

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -20,10 +20,19 @@ return { -- Collection of various small independent plugins/modules
 		--  You could remove this setup call if you don't like it,
 		--  and try some other statusline plugin
 		local statusline = require("mini.statusline")
-		-- set use_icons to true if you have a Nerd Font
-		statusline.setup({ use_icons = vim.g.have_nerd_font })
+
+		-- Your config table
+		local my_config = {
+			-- set use_icons to true if you have a Nerd Font
+			use_icons = vim.g.have_nerd_font,
+			set_vim_settings = true,
+		}
+
+		-- Setup with your config
+		statusline.setup(my_config)
 
 		-- Show only the file name (no path)
+		---@diagnostic disable-next-line: duplicate-set-field
 		statusline.section_filename = function()
 			local name = vim.fn.expand("%:t")
 			local modified = vim.bo.modified and " [+]" or ""
@@ -37,6 +46,31 @@ return { -- Collection of various small independent plugins/modules
 		statusline.section_location = function()
 			return "%2l:%-2v"
 		end
+
+		vim.api.nvim_set_hl(0, "MyStatusFilename", {
+			fg = "#C1C7E0", -- or whatever your theme uses for normal text
+			bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg, -- match editor bg
+		})
+
+		-- Style the statusline for inactive windows
+		---@diagnostic disable-next-line: duplicate-set-field
+		statusline.inactive = function()
+			vim.api.nvim_win_set_option(0, "winhighlight", "StatusLine:StatusLineNC")
+
+			return table.concat({
+				"", -- No mode
+				statusline.section_git(my_config),
+				statusline.section_diagnostics(my_config),
+				"%#MyStatusFilename#",
+				statusline.section_filename(my_config),
+				"%=",
+				"%#StatusLineNC#",
+				statusline.section_searchcount(my_config),
+				statusline.section_location(my_config),
+			}, " ")
+		end
+
+		vim.cmd([[highlight StatusLineNC guibg=#2F313D guifg=#A3AAC2]])
 
 		-- ... and there is more!
 		--  Check out: https://github.com/echasnovski/mini.nvim
